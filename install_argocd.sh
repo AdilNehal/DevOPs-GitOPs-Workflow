2d922c9cac192969d598dac22e0467ed458667ae
Adil Nehal <adilnehal2000@gmail.com>
2025-04-13 13:53:16 +0500

    Adding KEDA installation guide.

--- install_argocd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kubectl apply -n cd -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=stable
---
+++ install_tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl apply -n cd -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=stable
+
+# Install KEDA
+
+helm repo add kedacore https://kedacore.github.io/charts
+helm repo update
+helm install keda kedacore/keda --namespace keda --create-namespace
