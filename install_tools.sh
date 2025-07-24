a2371b665cde007d7a2a9e6498f2bdcd3b7129e9
Adil Nehal <adilnehal2000@gmail.com>
2025-07-24 18:28:27 +0500

    Adding installation guide for argo-rollouts.

--- install_tools.sh
+++ install_tools.sh
@@ -9,3 +9,12 @@ helm repo update
 helm install keda kedacore/keda --namespace keda --create-namespace
 
 #metric-server
+
+#Argo Rollouts
+kubectl apply -n argo-rollouts -f https://raw.githubusercontent.com/argoproj/argo-rollouts/stable/manifests/install.yaml
+
+#Kubectl argo rollouts plugin
+curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-linux-amd64
+chmod +x ./kubectl-argo-rollouts-linux-amd64
+sudo mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
+kubectl argo rollouts version
